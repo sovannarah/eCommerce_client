d916648ca890f2bd67fee092090886cf3c813adc
sovannarah <sovannara.hem@gmail.com>
2019-06-11 11:18:10 +0200

    changed structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import './App.css';
-import Menu from './img/icon/icon-menu.png';
-import Logo from './img/Utils-IMG/logo.png';
-import Account from './img/icon/icon-user.png'
-import Cart from './img/icon/icon-panier.png';
+import Header from './components/Header';
 
 function App() {
   return (
-    // <div className="App h-100">
-      <header id="header" className="container-fluid d-flex justify-content-between">
-        <div className="col-1 d-flex">
-          <img className="icon-logo m-auto" src={ Menu } />
-        </div>
-        <div className="h-100 d-flex">
-          <img className="h-75 m-auto" src={ Logo } />
-        </div>
-        <div className="ctn-acnt justify-content-between d-flex">
-            <img className="icon-cart mt-auto mb-auto" src={ Account } />
-            <img className="icon-cart mt-auto mb-auto" src={ Cart } />
-        </div>
-      </header>
-    // </div>
+    <div className="h-100">
+      <Header />
+    </div>
   );
 }
 
---
+++ client/src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Menu from '../img/icon/icon-menu.png';
+import Logo from '../img/Utils-IMG/logo.png';
+import Account from '../img/icon/icon-user.png'
+import Cart from '../img/icon/icon-panier.png';
+
+import '../style/css/header.css';
+
+class Header extends React.Component {
+
+    render() {
+        return(
+            <header id="header" className="d-flex justify-content-between">
+                <div className="col-1 d-flex">
+                    <img className="icon-logo m-auto" src={ Menu } />
+                </div>
+                <div className="h-100 d-flex">
+                    <img className="h-75 m-auto" src={ Logo } />
+                </div>
+                <div className="ctn-acnt justify-content-between d-flex">
+                    <img className="icon-cart mt-auto mb-auto" src={ Account } />
+                    <img className="icon-cart mt-auto mb-auto" src={ Cart } />
+                </div>
+            </header>
+        );
+    }
+
+}
+
+export default Header;
