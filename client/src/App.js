2e7bd85aa48925489430977657110b4184480c6e
sovannarah <sovannara.hem@gmail.com>
2019-06-11 10:54:40 +0200

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Menu from './img/icon/icon-menu.png';
+import Logo from './img/Utils-IMG/logo.png';
+import Account from './img/icon/icon-user.png'
+import Cart from './img/icon/icon-panier.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    // <div className="App h-100">
+      <header id="header" className="container-fluid d-flex justify-content-between">
+        <div className="col-1 d-flex">
+          <img className="icon-logo m-auto" src={ Menu } />
+        </div>
+        <div className="h-100 d-flex">
+          <img className="h-75 m-auto" src={ Logo } />
+        </div>
+        <div className="ctn-acnt justify-content-between d-flex">
+            <img className="icon-cart mt-auto mb-auto" src={ Account } />
+            <img className="icon-cart mt-auto mb-auto" src={ Cart } />
+        </div>
       </header>
-    </div>
+    // </div>
   );
 }
 
