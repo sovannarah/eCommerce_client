c87723a469e054755951793dfbeae029ff4d2ff2
ouzepe <je.ouzepe@gmail.com>
2019-06-11 15:26:18 +0200

    login & register done, stay sync with api

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Login from './components/Login';
+import Register from './components/Register';
+
 
 function App() {
   return (
     <div className="h-100">
       <Header />
+      <Login/>
     </div>
   );
 }
---
+++ client/src/components/Register.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import Menu from '../img/icon/icon-menu.png';
+import Logo from '../img/Utils-IMG/logo.png';
+import Account from '../img/icon/icon-user.png'
+import Cart from '../img/icon/icon-panier.png';
+
+import '../style/css/header.css';
+
+class Register extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { isLoading: true }
+    }
+
+    componentDidMount() {
+        return fetch('10.34.7.0:8000/register')
+            .then((response) => response.json())
+            .then((responseJson) => {
+
+                this.setState({
+                    isLoading: false,
+                    dataSource: responseJson.register,
+                }, function () {
+
+                });
+
+            })  
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+    render() {
+        return (
+            <div className="login">
+                <div className="title">
+                    <h1>Register</h1>
+                </div>
+                <div className="form">
+                    <form action="" method="post">
+                        <div className="form-group">
+                            <label for="email">Email:</label>
+                            <input
+                                type="text"
+                                class="form-control"
+                                name="email"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label for="email">Password:</label>
+                            <input
+                                type="password"
+                                class="form-control"
+                                name="password"
+                            />
+                        </div>
+                        <button className="btn btn-dark" type>Subscribe</button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default Register;
