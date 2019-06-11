d49b0ee9b6f145fbce2c9bc609074007a0648026
Vulgaris <mike_b2000@yahoo.com>
2019-06-11 18:53:07 +0200

    working on the description page of product

--- client/src/components/Description.js
+++ client/src/components/Description.js
@@ -5,10 +5,25 @@ import '../style/css/header.css';
 import '../style/css/description.css';
 
 class Description extends React.Component {
+    constructor(props){
+        super(props);
+        this.data = {
+            title: "Boss",
+            model: "The sent",
+            price: 12,
+            desc: 'wtf'
+        };
+    }
     render() {
+        const {title, model, price, desc} = this.data;
         return (
 
-                <div className="desc"><p>ASjfkjfhaksj</p></div>
+                <div className="desc">
+                    <h2>{title}</h2>
+                    <span>{model}</span>
+                    <div>{price}</div>
+                    <p>{desc}</p>
+                </div>
 
         );
     }
