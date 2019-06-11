0a6e470cace3bb63e6bd7aa19cd4d96fe6dcb3c2
Vulgaris <mike_b2000@yahoo.com>
2019-06-11 15:41:45 +0200

    working on the description page of product

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Description from './components/Description';
 
 function App() {
-  return (
-    <div className="h-100">
-      <Header />
-    </div>
-  );
+    return (
+        <div className="h-100">
+            <Header/>
+            <section className="stn-app d-flex">
+                <Description/>
+            </section>
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/components/Description.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+import '../style/css/header.css';
+import '../style/css/description.css';
+
+class Description extends React.Component {
+    render() {
+        return (
+
+                <div className="desc"><p>ASjfkjfhaksj</p></div>
+
+        );
+    }
+}
+
+export default Description;
