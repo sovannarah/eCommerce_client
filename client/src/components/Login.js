8c7a2c33af484bc5558c19b10856d5791a2f8903
ouzepe <je.ouzepe@gmail.com>
2019-06-13 09:15:50 +0200

    register login done left api

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,6 +3,9 @@ import Menu from '../img/icon/icon-menu.png';
 import Logo from '../img/Utils-IMG/logo.png';
 import Account from '../img/icon/icon-user.png'
 import Cart from '../img/icon/icon-panier.png';
+import Cartt from '../img/icon/icon-black-panier.png';
+import account from '../img/icon/icon.png';
+import Smoke from '../img/icon/smoke.png';
 // import axios from 'axios';
 
 import '../style/css/header.css';
@@ -10,54 +13,87 @@ import '../style/css/header.css';
 class Login extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { isLoading: true }
+        this.state = {
+            sessionToken: null,
+            email: '',
+            password: ''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+    componentDidMount() {
+        this.setState({ isLoading: true });
+
+        fetch("10.34.7.0:8001/login")
+            .then(response => response.json())
+            .then(data => this.setState({
+                email: "",
+                password: "",
+                isLoading: false
+            }))
+            .catch(error => this.setState({ error, isLoading: false }));
     }
 
-    componentDidMount() {
-        return fetch('10.34.7.0:8000/login')
-            .then((response) => response.json())
-            .then((responseJson) => {
-
-                this.setState({
-                    isLoading: false,
-                    dataSource: responseJson.login,
-                }, function () {
-
-                });
-
-            })
-            .catch((error) => {
-                console.error(error);
+    handleSubmit(e) {
+        e.preventDefault();
+        this.oktaAuth.signIn({
+            email: this.state.email,
+            password: this.state.password
+        })
+            .then(res => this.setState({
+                sessionToken: res.sessionToken
+            }))
+            .catch(err => {
+                this.setState({ error: err.message });
+                console.log(err.statusCode + ' error', err)
             });
     }
+    handleEmailChange(e) {
+        this.setState({ email: e.target.value });
+    }
 
+    handlePasswordChange(e) {
+        this.setState({ password: e.target.value });
+    }
 
     render() {
         return (
-            <div className="login">
-                <div className="title">
-                    <h1>Login</h1>
+            <div>
+                <div className="login-img">
+                    <img src={Smoke} />
                 </div>
-                <div className="form">
-                    <form action="" method="post">
-                        <div className="form-group">
-                            <label for="email">Email:</label>
-                            <input
-                                type="text"
-                                class="form-control"
-                                name="email"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label for="email">Password:</label>
-                            <input
-                                type="password"
-                                class="form-control"
-                                name="password"
-                            />
-                        </div>
-                        <button className="btn btn-dark" type>Login</button>
-                    </form>
+                <div className="login">
+                    <div className="icon-login">
+                        <img className="icon-cart mt-auto mb-auto" src={account} />
+                        <img className="icon-cart mt-auto mb-auto" src={Cartt} />
+                    </div>
+                    <div className="title">
+                        <h1>Login</h1>
+                    </div>
+                    <div className="form">
+                        <form action="" method="post">
+                            <div className="form-group">
+                                <label for="email">Email:</label>
+                                <input
+                                    onChange={this.handleEmailChange}
+                                    type="text"
+                                    class="form-control"
+                                    name="email"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label for="email">Password:</label>
+                                <input
+                                    onChange={this.handlePasswordChange}
+                                    type="password"
+                                    class="form-control"
+                                    name="password"
+                                />
+                            </div>
+                            <button className="btn btn-dark" type>Login</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         );
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -3,6 +3,9 @@ import Menu from '../img/icon/icon-menu.png';
 import Logo from '../img/Utils-IMG/logo.png';
 import Account from '../img/icon/icon-user.png'
 import Cart from '../img/icon/icon-panier.png';
+import Cartt from '../img/icon/icon-black-panier.png';
+import account from '../img/icon/icon.png';
+import Smoke from '../img/icon/smoke.png';
 
 import '../style/css/header.css';
 
@@ -12,49 +15,59 @@ class Register extends React.Component {
         this.state = { isLoading: true }
     }
 
-    componentDidMount() {
-        return fetch('10.34.7.0:8000/register')
-            .then((response) => response.json())
-            .then((responseJson) => {
+    onClick = async () => {
 
-                this.setState({
-                    isLoading: false,
-                    dataSource: responseJson.register,
-                }, function () {
-
-                });
-
-            })  
-            .catch((error) => {
-                console.error(error);
-            });
+        await fetch('10.34.7.0:8001/register', {
+            method: "POST",
+            body: JSON.stringify({
+                email: this.state.email,
+                password: this.state.password,
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then((response) => response.json()).then((responseJson) => {
+            console.log(responseJson);
+            // Actions.home();
+        })
     }
+
     render() {
         return (
-            <div className="login">
-                <div className="title">
-                    <h1>Register</h1>
+
+            <div>
+                <div className="login-img">
+                    <img src={Smoke} />
                 </div>
-                <div className="form">
-                    <form action="" method="post">
-                        <div className="form-group">
-                            <label for="email">Email:</label>
-                            <input
-                                type="text"
-                                class="form-control"
-                                name="email"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label for="email">Password:</label>
-                            <input
-                                type="password"
-                                class="form-control"
-                                name="password"
-                            />
-                        </div>
-                        <button className="btn btn-dark" type>Subscribe</button>
-                    </form>
+                <div className="login">
+                    <div className="icon-login">
+                        <img className="icon-cart mt-auto mb-auto" src={account} />
+                        <img className="icon-cart mt-auto mb-auto" src={Cartt} />
+                    </div>
+                    <div className="title">
+                        <h1>Register</h1>
+                    </div>
+                    <div className="form">
+                        <form action="" method="post">
+                            <div className="form-group">
+                                <label for="email">Email:</label>
+                                <input
+                                    type="text"
+                                    class="form-control"
+                                    name="email"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label for="email">Password:</label>
+                                <input
+                                    type="password"
+                                    class="form-control"
+                                    name="password"
+                                />
+                            </div>
+                            <button className="btn btn-dark" type>Subscribe</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         );
