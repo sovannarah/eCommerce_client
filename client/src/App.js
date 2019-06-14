4e3eeb9e035a3529768830d116cc1f985da45602
Vulgaris <mike_b2000@yahoo.com>
2019-06-14 13:24:18 +0200

    basic description page / stock to be added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import Description from './components/Description';
 
 class App extends React.Component {
 
 
     render() {
+        return (<Description/>);
 
     }
 }
--- client/src/components/Description.js
+++ client/src/components/Description.js
@@ -1,71 +1,40 @@
 import React from 'react';
 
-
-
-
+const DEFAULT_PRODUIT = {
+    brand: 'BOSS',
+    sndName: 'Mike the Best <3 ',
+    description: '"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."\n' +
+        '\n',
+    price: '15,23',
+    image: '',
+    stock: 10,
+};
 
 class Description extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             produit: null,
-            isOpen: false
-        }
-        this.containerDiv = React.createRef();
+        };
     }
 
-    componentWillMount() {
-        document.addEventListener('mousedown', this.handleMouseDown);
-    }
-
-    componentWillUnmount() {
-        document.removeEventListener('mousedown', this.handleMouseDown);
-    }
-
-    handleMouseDown = e => {
-        if (this.containerDiv.current.contains(e.target))
-            return;
-        this.close();
-    }
-
-    open = item => {
-
-        //TODO: API call to get item data
-        this.setState({
-            produit: item,
-            isOpen: true
-        })
-    };
-
-    close = () => {
-        this.setState({
-            isOpen: false
-        });
-    };
 
     render() {
-        var brand= this.props.brand;
-        var sndName = this.props.sndName;
-        var price = this.props.price;
-        var description = this.props.description;
-
+        const {brand, sndName, price, description, stock} = (this.state.produit || DEFAULT_PRODUIT);
         return (
 
-            <div ref={this.containerDiv} id='desc' className='desc'>
-                <div style={{ width: '43vw' }}>
-                    <div className='ctn-brand d-flex flex-column'>
-                    <h3 className={"title"}>{brand}</h3>
-                    <h3 className={"model"}>&#8212; {sndName} &#8212;</h3>
+            <div>
+                <div>
+                    <h3>{brand}</h3>
+                    <h3>&#8212; {sndName} &#8212;</h3>
+                    <div>&euro; {price}</div>
+                    <div>
+                        <div> stock: {stock}</div>
+                        <button>TO CART</button>
                     </div>
-                    <div className={"price_font"}>&euro; {price}</div>
-                    
-                        <button className="justify-content-center">ADD TO CART</button>
-                    
-                    <div className={"descript"}>
-                        <p className={"bold"}>DESCRIPTION</p>
-                        <p>{description}</p>
-                        </div>
-
+                    <div>
+                        <div>DESCRIPTION</div>
+                        {description}</div>
                 </div>
             </div>
 
