e02da66ebf3a37b8b03db6443b1a5077539b75ee
Vulgaris <mike_b2000@yahoo.com>
2019-06-17 14:41:54 +0200

    merge??

--- client/src/App.js
+++ client/src/App.js
@@ -4,37 +4,21 @@ import Header from './components/Header';
 import Menu from './components/Menu';
 import Routeur from './Routeur';
 import './App.css';
-import Menu from './components/Menu';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import AllArticles from './components/Menu/AllArticles';
 
-<<<<<<< HEAD
 class App extends React.Component {
 
   render() {
     return (
-      <div id="main-div" className="h-100">
-        <BrowserRouter>
-          <Menu />
-          <Header />
-          <div id="margin"></div>
-          <Routeur />
-        </BrowserRouter>
-      </div>
-    );
-  }
-=======
-
-function App() {
-    return (
-        <div className="h-100">
-            <Router>
-                <Menu />
-            </Router>
+        <div id="main-div" className="h-100">
+          <BrowserRouter>
+            <Menu />
+            <Header />
+            <div id="margin"></div>
+            <Routeur />
+          </BrowserRouter>
         </div>
     );
->>>>>>> menu
+  }
 }
-
 
 export default App;
