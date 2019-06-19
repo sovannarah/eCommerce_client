20d10acebf53c392b1060e5e6c26db0e08672630
sovannarah <sovannara.hem@gmail.com>
2019-06-20 01:27:30 +0200

    refonte article et travail bibiMaster

--- client/src/Routeur.js
+++ client/src/Routeur.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Articles from './components/Articles';
 import Article from './components/Article';
 import Register from './components/Register';
-import Admin from './components/Admin';
+import Admin from './components/AdminUpdate';
+import AdminCreate from './components/Admin';
 import { Switch, Route } from 'react-router';
 
 import Home from './components/Home';
@@ -10,13 +11,14 @@ import Home from './components/Home';
 class Routeur extends React.Component {
     render() {
         return (
-                <Switch>
-                    <Route exact path="/" component={ Home } />
-                    <Route path="/articles" component={ Articles } />
-                    <Route path="/article/:id" component={ Article } />
-                    <Route path="/register" component={ Register } />
-                    <Route path="/admin" component={ Admin } />
-                </Switch>
+            <Switch>
+                <Route exact path="/" component={ Home } />
+                <Route path="/articles" component={ Articles } />
+                <Route path="/article/:id" component={ Article } />
+                <Route path="/register" component={ Register } />
+                <Route exact path="/admin" component={ Admin } />
+                <Route exact path="/admin/create" component={ AdminCreate } />
+            </Switch>
         )
     }
 }
