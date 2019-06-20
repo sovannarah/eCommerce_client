121cbf2dba0e9270fcde5e1d45b16c4fca88ed06
sovannarah <sovannara.hem@gmail.com>
2019-06-20 10:19:19 +0200

    refont admin

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import TextField from "@material-ui/core/TextField";
 import Fab from "@material-ui/core/Fab";
+import '../style/css/admin.css';
 
 class Admin extends React.Component {
     constructor(props) {
@@ -62,63 +63,80 @@ class Admin extends React.Component {
 
     render() {
         return (
-            <section className="col-12 h-10 d-flex">
+            <section id="stn-adminCreate" className="col-12 h-100 d-flex">
 
-                <form id="form-add" method="post"
-                      className="bg-light d-flex justify-content-around flex-column col-md-6 h-50 m-auto">
-                    <p></p>
-                    <p></p>
-                    <TextField
-                        id="outlined-name"
-                        label="title"
-                        name="title"
-                        type="string"
-                        onChange={this.change}
-                    />
-                    <TextField
-                        id="outlined-name"
-                        label="description"
-                        name="description"
-                        type="string"
-                        onChange={this.change}
+                <form className="m-auto" id="form-add" method="post"
+                className="d-flex col-md-12 h-50 m-auto">
+                    <div className="d-flex flex-column mh-100 col-12">
+                        <div className="d-flex h-75 m-5">
+                            <div className="col-6 d-flex flex-column ">
+                                <label className="d-flex flex-column">Title
+                                    <input
+                                        id="outlined-name"
+                                        label="title"
+                                        name="title"
+                                        type="string"
+                                        onChange={this.change}
+                                    />
+                                </label>
+                                <label className="d-flex flex-column">Description
+                                    <input
+                                        id="outlined-name"
+                                        label="description"
+                                        name="description"
+                                        type="string"
+                                        onChange={this.change}
 
-                    />
-                    <TextField
-                        id="outlined-name"
-                        label="price"
-                        name="price"
-                        type="integer"
-                        onChange={this.change}
+                                    />
+                                </label>
+                                <label className="d-flex flex-column">Price
+                                    <input
+                                        id="outlined-name"
+                                        label="price"
+                                        name="price"
+                                        type="integer"
+                                        onChange={this.change}
 
-                    />
-                    <TextField
-                        id="outlined-name"
-                        label="image"
-                        name="image"
-                        type="file"
-                        onChange={this.change}
+                                    />
+                                </label>
+                            </div>
+                            <div className="col-6 d-flex justify-content-around flex-column">
+                                <label className="d-flex flex-column">Images
+                                    <input
+                                        id="outlined-name"
+                                        label="image"
+                                        name="image"
+                                        type="file"
+                                        onChange={this.change}
+                                    />
+                                </label>
+                                <label className="d-flex flex-column">Stock
+                                    <input
+                                        id="outlined-name"
+                                        label="stock"
+                                        name="stock"
+                                        type="integer"
+                                        onChange={this.change}
 
-                    />
-                    <TextField
-                        id="outlined-name"
-                        label="stock"
-                        name="stock"
-                        type="integer"
-                        onChange={this.change}
+                                    />
+                                </label>
+                                <label className="d-flex flex-column">Category
+                                    <input
+                                        id="outlined-name"
+                                        label="category"
+                                        name="category"
+                                        type="string"
+                                        onChange={this.change}
 
-                    />
-                    <TextField
-                        id="outlined-name"
-                        label="category"
-                        name="category"
-                        type="string"
-                        onChange={this.change}
-
-                    />
-                    <Fab id="button-add" onClick={this.addItem} type="submit" className="w-50 ml-auto mr-auto"
-                         variant="extended" color="secondary">
-                        Add Item
-                    </Fab>
+                                    />
+                                </label>
+                            </div>
+                        </div>
+                        <Fab id="button-add" onClick={this.addItem} type="submit" className="w-25 ml-auto "
+                            variant="extended" color="secondary">
+                            Add Item
+                        </Fab>
+                    </div>
                 </form>
             </section>
         );
