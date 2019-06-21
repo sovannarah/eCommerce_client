03c820303f4c3a99c099c6c0286ca6ddae970301
ouzepe <je.ouzepe@gmail.com>
2019-06-21 11:26:08 +0200

    push test

--- client/src/components/userCtrl.js
+++ client/src/components/userCtrl.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-let ip='http://10.34.7.68:8000';
+let ip = 'http://10.34.6.23:8000';
 
 class UserCtrl extends React.Component {
 
@@ -19,21 +19,21 @@ class UserCtrl extends React.Component {
     }
 
     changeEmail(e) {
-        this.setState({email: e.target.value});
+        this.setState({ email: e.target.value });
     }
 
     changePassword(e) {
-        this.setState({password: e.target.value});
+        this.setState({ password: e.target.value });
     }
 
     login(e) {
         e.preventDefault();
-        axios.post(ip + '/login', {email: this.state.email, password: this.state.password})
-            .then(res=> {
+        axios.post(ip + '/login', { email: this.state.email, password: this.state.password })
+            .then(res => {
                 const user = res.data;
                 console.log(res.data);
-                if(user.token) {
-                    localStorage.setItem('token' , user.token);
+                if (user.token) {
+                    localStorage.setItem('token', user.token);
                     localStorage.setItem('email', user.email);
                 }
             })
@@ -45,7 +45,7 @@ class UserCtrl extends React.Component {
 
     render() {
 
-        if(this.props.user) {
+        if (this.props.user) {
             return (
                 <div id="menu-user" className="d-flex flex-column justify-content-around bg-light open">
                     <Link to="/admin">My Account</Link>
@@ -55,22 +55,22 @@ class UserCtrl extends React.Component {
         } else {
             return (
                 <div id="menu-user" className="d-flex flex-column justify-content-around bg-light open">
-                    <input id="input-username" 
-                    className="col-12" 
-                    onChange={this.changeEmail} 
-                    placeholder="Email  " 
-                    type="text" />
-                    <input id="input-password" 
-                    className="col-12" 
-                    onChange={this.changePassword} 
-                    type="password" 
-                    placeholder="password"
+                    <input id="input-username"
+                        className="col-12"
+                        onChange={this.changeEmail}
+                        placeholder="Email  "
+                        type="text" />
+                    <input id="input-password"
+                        className="col-12"
+                        onChange={this.changePassword}
+                        type="password"
+                        placeholder="password"
                     />
-                    <input id="connect-button" 
-                    onClick={this.login} 
-                    value="Connection" 
-                    className="btn-default bg-mainly" 
-                    type="submit" 
+                    <input id="connect-button"
+                        onClick={this.login}
+                        value="Connection"
+                        className="btn-default bg-mainly"
+                        type="submit"
                     />
                     <p>Not registered? <Link to="/register">Sign In</Link></p>
                 </div>
