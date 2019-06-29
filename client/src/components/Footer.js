64bd6d5acd71e2e0fcd0629d3400aef51891b08b
sovannarah <sovannara.hem@gmail.com>
2019-06-29 14:22:29 +0200

    erreur dortographe

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,7 +7,7 @@ class Footer extends React.Component {
         return (
             <footer className="container-fluid d-flex justify-content-center">
                 <div className="text-light d-flex flex-column justify-content-center">
-                    <h4 className="">Sign up to receive special offers ans update</h4>
+                    <h4 className="">Sign up to receive special offers and update</h4>
                     <form id="form-footer" method="post" className="d-flex mt-5 mb-5 d-flex justify-content-center">
                         <input type="text" className="col-7" placeholder="Enter your email adress"/>
                         <button className="col-4 ml-2 bg-mainly" type="submit">Sign up !</button>
