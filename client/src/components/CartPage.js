b9d3dc1dccf9d8b073dd6800f92976ebe07a00d9
sovannarah <sovannara.hem@gmail.com>
2019-07-01 12:28:07 +0200

    merge fix

--- client/src/components/CartPage.js
+++ client/src/components/CartPage.js
@@ -3,6 +3,7 @@ import Table from 'react-bootstrap/Table';
 import Axios from 'axios';
 import {Link} from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
 import AddressForm from './Addresse/AddressForm';
 
 import {FormControl} from 'react-bootstrap';
@@ -29,31 +30,31 @@ class Cart extends Component {
 		this.displayAdress = this.displayAdress.bind(this);
 	}
 
-    componentDidMount() {
-        let allUpdated = true;
-        let copyPrice = this.state.HT;
-        getCart().forEach(
-            (article) => {
-                Axios.get(apiArticleURI + article.id)
-                    .then((res) => {
-                        const updatedArticle = {...article, erased: false, ...res.data};
-                        this.updateArticle(updatedArticle);
-                    })
-                    .catch((error) => {
-                        if (error.response && error.response.status === 404) {
-                            article.erased = true;
-                        } else {
-                            console.error(error);
-                            allUpdated = false;
-                        }
-                        this.updateArticle(article);
-                    });
-                copyPrice += (article.price * article.quantity)
-            }
-        );
-        this.setState({updated: allUpdated});
-        this.setState({HT: copyPrice});
-    }
+	componentDidMount() {
+		let allUpdated = true;
+		let copyPrice = this.state.HT;
+		getCart().forEach(
+			(article) => {
+				Axios.get(apiArticleURI + article.id)
+					.then((res) => {
+						const updatedArticle = {...article, erased: false, ...res.data};
+						this.updateArticle(updatedArticle);
+					})
+					.catch((error) => {
+						if (error.response && error.response.status === 404) {
+							article.erased = true;
+						} else {
+							console.error(error);
+							allUpdated = false;
+						}
+						this.updateArticle(article);
+					});
+				copyPrice += (article.price*article.quantity)
+			}
+		);
+		this.setState({updated: allUpdated});
+		this.setState({HT: copyPrice});
+	}
 
 
 
@@ -82,46 +83,46 @@ class Cart extends Component {
 	 * Removes article from state by id, and updates sessionStorage
 	 * @param id
 	 */
-	removeArticle = (id) => { 
+	removeArticle = (id) => {
 		this.setState((prevState) => {
 			const articles = prevState.articles.filter(article => article.id !== id);
-			 sessionStorage.setItem(storageKey, JSON.stringify(articles));
+			sessionStorage.setItem(storageKey, JSON.stringify(articles));
 			return {articles};
 		});
 	};
 
-    /**
-     * TODO: understand how transport fee will work
-     */
-    calculateTranFee = () => {
-        // $this.setState({fee: 20});
-        return (20);
-    };
+	/**
+	 * TODO: understand how transport fee will work
+	 */
+	calculateTranFee = () => {
+		// $this.setState({fee: 20});
+		return (20);
+	}
 
-    render() {
-        return (
-            <section id="ctn-cartPage" className=" min-vh-100 flex-column container-fluid d-flex">
-                <div id="ctn-cart" className="col-md-9 m-auto ">
-                    <Table variant='dark'>
-                        <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Unit Price</th>
-                            <th>Qt.</th>
-                            <th>Stock</th>
-                        </tr>
-                        </thead>
-                        <tbody>
+	render() {
+		return (
+			<section id="ctn-cartPage" className=" min-vh-100 flex-column container-fluid d-flex">
+				<div id="ctn-cart" className="col-md-9 m-auto ">
+					<Table variant='dark'>
+						<thead>
+							<tr>
+								<th>Name</th>
+								<th>Unit Price</th>
+								<th>Qt.</th>
+								<th>Stock</th>
+							</tr>
+						</thead>
+						<tbody>
 
-                        {this.state.articles.map((article, index) =>
-                            <>
-                                <Article key={index} article={article}
-                                         delete={this.removeArticle}
-                                         update={this.updateArticle}
-                                />
-                            </>
-                        )}
-                        </tbody>
+						{this.state.articles.map((article, index) => 
+						<>
+							<Article key={index} article={article}
+								delete={this.removeArticle}
+								update={this.updateArticle}
+							/>
+						</>
+						)}
+						</tbody>
 
 						
 					</Table>
@@ -130,75 +131,75 @@ class Cart extends Component {
 							<h2>Localisation</h2>
 							<h4 onClick={this.displayAdress}>Add Adress</h4>
 						</div>
-					</div>
-                    <div className="container">
-                        <AddressForm/>
-                    </div>
-                    <div className="d-flex bg-grey2 justify-content-between w-100">
-                        <h5 className="text-light mt-auto mb-auto ml-3">SOUS TOTAL : {this.state.HT}</h5>
-                    </div>
-                    <div className="d-flex bg-grey2 justify-content-between w-100">
-                        <h5 className="text-light mt-auto mb-auto ml-3">TRANSPORT FEE : </h5>
-                    </div>
-                    <div className="d-flex bg-grey2 justify-content-between w-100">
-                        <h5 className="text-light mt-auto mb-auto ml-3">TOTAL PRICE : {this.state.HT}</h5>
-                        <Link to="/checkin">
-                            <button className="btn-mainly mr-2 mt-2 mb-2">PAY</button>
-                        </Link>
-                    </div>
-                </div>
 
-            </section>
-        );
-    }
+						<AddressForm />
+
+					</div>
+					<div className="d-flex bg-grey2 justify-content-between w-100">
+						<h5 className="text-light mt-auto mb-auto ml-3">SOUS TOTAL : {this.state.HT}</h5>
+					</div>
+					<div className="d-flex bg-grey2 justify-content-between w-100">
+						<h5 className="text-light mt-auto mb-auto ml-3">TRANSPORT FEE : </h5>
+					</div>
+					<div className="d-flex bg-grey2 justify-content-between w-100">
+						<h5 className="text-light mt-auto mb-auto ml-3">TOTAL PRICE : {this.state.ht}</h5>
+						<Link to="/checkin">
+							<button className="btn-mainly mr-2 mt-2 mb-2">PAY</button>
+						</Link>
+					</div>
+				</div>
+				
+			</section>
+		);
+	}
 }
 
 
 function Article(props) {
-    const {id, stock, title, price, erased, quantity} = props.article;
-    const outOfStock = stock === 0;
-    const stockStr = erased ?
-        'NaN' :
-        stock !== null ?
-            stock :
-            'unknown';
-    return (
-        <tr className={outOfStock ? 'disabled text-muted' : ''}>
-            <td>
-                {!erased ?
-                    <Link className="text-light" to={`/article/${id}`}
-                          disabled={outOfStock}
-                    >
-                        {title}
-                    </Link> :
-                    <span className='text-muted'>
+	const {id, stock, title, price, erased, quantity} = props.article;
+	const outOfStock = stock === 0;
+	const stockStr = erased ?
+		'NaN' :
+		stock !== null ?
+			stock :
+			'unknown';
+	return (
+		<tr className={outOfStock ? 'disabled text-muted' : ''}>
+			<td>
+				{!erased ?
+					<Link className="text-light" to={`/article/${id}`}
+						  disabled={outOfStock}
+					>
+						{title}
+					</Link> :
+					<span className='text-muted'>
 						(No longer available) {title}
 					</span>
-                }
-            </td>
-            <td>{price}</td>
-            <td>
-                <FormControl type='number'
-                             className="text-dark"
-                             placeholder={quantity}
-                             min='0' max={stock}
-                             disabled={outOfStock || erased}
-                             onChange={(ev) => {
-                                 props.update({...props.article, quantity: ev.target.value});
-                             }}
-                             value={quantity}/>
-            </td>
-            <td>{stockStr}</td>
-            <td>
-                <Button variant='danger'
-                        className='material-icons'
-                        onClick={() => props.delete(id)}
-                >
-                    delete
-                </Button>
-            </td>
-        </tr>
-    );
+				}
+			</td>
+			<td>{price}</td>
+			<td>
+				<FormControl type='number'
+							 className="text-dark" 
+						  	 placeholder={quantity} 
+							 min='0' max={stock}
+							 disabled={outOfStock || erased}
+							 onChange={(ev) => {
+								 props.update({...props.article, quantity: ev.target.value});
+							 }}
+							 value={quantity}/>
+			</td>
+			<td>{stockStr}</td>
+			<td>
+				<Button variant='danger'
+						className='material-icons'
+						onClick={() => props.delete(id)}
+				>
+					delete
+				</Button>
+			</td>
+		</tr>
+	);
 }
 
 
@@ -206,8 +207,8 @@ function Article(props) {
  * @returns {Array}
  */
 function getCart() {
-    const json = sessionStorage.getItem(storageKey);
-    return json ? JSON.parse(json) : [];
+	const json = sessionStorage.getItem(storageKey);
+	return json ? JSON.parse(json) : [];
 }
 
 /**
@@ -216,15 +217,15 @@ function getCart() {
  * @param quantity
  */
 function addToCart(article, quantity) {
-    const cart = getCart();
-    let existing = cart.find(oldArticle => oldArticle.id === article.id);
-    if (!existing) {
-        existing = article;
-        existing.quantity = 0;
-        cart.push(existing);
-    }
-    existing.quantity += quantity;
-    sessionStorage.setItem(storageKey, JSON.stringify(cart));
+	const cart = getCart();
+	let existing = cart.find(oldArticle => oldArticle.id === article.id);
+	if (!existing) {
+		existing = article;
+		existing.quantity = 0;
+		cart.push(existing);
+	}
+	existing.quantity += quantity;
+	sessionStorage.setItem(storageKey, JSON.stringify(cart));
 }
 
 export {Cart as default, addToCart};
